8164536e297acee93b1504b8c9a95523ddbb133e
Olioliviaa <oliviawei1001@gmail.com>
2026-09-10 19:01:27 -0700

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -14,7 +14,7 @@ const myRecipesData = [
     "图片": "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/50/Male_gorilla_in_SF_zoo.jpg/960px-Male_gorilla_in_SF_zoo.jpg?utm_source=zh-yue.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail",
     "所需食材": "高筋面粉 250g, 无盐黄油 120g",
     "步骤": "1. 揉出薄膜。| 2. 第一次发酵60分钟。| 3. 二发后180度烘烤25分钟。"
-  }
+  },
     {
     "分类": "火锅",
     "菜名": "椰子鸡火锅",
