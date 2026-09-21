4ae2d74eb22ec05b9cd21230c0bf19364f995327
Olioliviaa <oliviawei1001@gmail.com>
2026-09-21 15:12:40 -0700

    Update recipes data with new dish and ingredients

--- recipes.js
+++ recipes.js
@@ -2,11 +2,11 @@
 // 食材之间用【英文逗号】隔开，步骤之间用【英文竖线 |】隔开
 const myRecipesData = [
   {
-    "分类": "海鲜",
-    "菜名": "避风塘炒虾",
-    "图片": "./images/xia.jpg",
-    "所需食材": "大虾 500g, 面包糠 50g, 大蒜 1整头",
-    "步骤": "1. 大虾腌制10分钟。| 2. 炸蒜末至金黄。| 3. 下面包糠大虾翻炒均匀。"
+    "分类": "肉类",
+    "菜名": "牛肉丸",
+    "图片": "https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02087bf4a1a23c92e5d3572c11df6216?x-bce-process=image/format,f_auto/quality,Q_70/resize,m_lfit,limit_1,w_536",
+    "所需食材": "90%牛肉末 500g, 木薯淀粉Tapioca 15g, 小苏打 1g, 泡打粉 1g, 冰块, 盐 10g, 调味料",
+    "步骤": "将肉和冰块放入绞肉机里搅打| 加粉类继续搅打，过热就放入冰块或放入冰箱冷却| 调味后用筷子搅拌"
   },
   {
     "分类": "烘焙",
@@ -16,9 +16,9 @@ const myRecipesData = [
     "步骤": "1. 揉出薄膜。| 2. 第一次发酵60分钟。| 3. 二发后180度烘烤25分钟。"
   },
     {
-    "分类": "火锅",
-    "菜名": "椰子鸡火锅",
-    "图片": "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/50/Male_gorilla_in_SF_zoo.jpg/960px-Male_gorilla_in_SF_zoo.jpg?utm_source=zh-yue.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail",
+    "分类": "海鲜",
+    "菜名": "避风塘炒虾",
+    "图片": "./images/xia.jpg",
     "所需食材": "椰青 1个, 椰子水 1瓶, 鸡肉, 水",
     "步骤": "1. 切开椰子取其果肉切块。| 2. 将椰肉、椰子水、鸡肉和水一起倒入锅中。| 3. 下入其他喜欢吃的食材煮熟后即可食用。"
   }
